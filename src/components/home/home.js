d7615e563420bca6fdd2cfe0cdf0ed07310c7e30
Rahul Chaudhary <rahul.chaudhary@algoworks.com>
2022-03-16 13:33:19 +0530

    Home UI updated

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,40 +1,52 @@
 import * as React from "react";
 
-
 const Home=()=>{
     return (
-        // <div className="home-bg flex-center-center">
-        //     <div>
-        //         <h1>Welcome to Home</h1>
-        //     </div>
-        // </div>
-        <header className="head">
-            <div className="head-details  mt-100">
-                <div className="main-details">
-                    <h1 className="main-heading pr-10">
-                        <p className="main-heading-text text-to-right"> Digital Business Cards </p>
-                    </h1>
-                    <p className="sub-heading"> <span className="text-to-right ls0-12"> THE NEXT GENERATION OF NETWORKING </span></p>
-                    <p className="header-details-1">
-                        <span className="text-to-righ"> BusiCard lets you get the most out of your business card. It allows you
-                            to share your business card digitally, linking to all of your social media and website
+
+        <div className="home-bg">
+            <div className="home-main">
+                <div className="home-left-div mt-80">
+                    <div className="heading-div">
+                        <h1>Digital Business Cards</h1>
+                    </div>
+                    <div className="content-div">
+                        <span className="content-1">
+                            THE NEXT GENERATION OF NETWORKING
                         </span>
-                        <span className="text-to-righ">
+                        <span  className="content-2">
+                            <p>
+                            BusiCard lets you get the most out of your business card. It allows you
+                            to share your business card digitally, linking to all of your social media and website
+                            </p>
+                        </span>
+                        <span  className="content-3">
+                            <p>
                             With BusiCard, managing your contacts has never been easier. Sort your contacts in custom
                             categories that fit your business needs. BusiCard even lets you see what links your leads are
                             most interested in.
+                            </p>
                         </span>
-                    </p>
+                    </div>
                 </div>
+                <div className="home-right-div mt-80">
+                    <div >
+                        <img src={require('../../assets/images/home_vector_img.png')} alt=""/>
+                    </div>
+                </div>
+                <div className="home-bottom-div">
+                    <div className="home-bottom-part-1">
+                        <a href="/"> EXPLORE THE POSSIBILITIES</a>
+                    </div>
+                    <div  className="home-bottom-part-2">
+                        
+                    </div>
+                    <div  className="home-bottom-part-3">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                <div className="vector-img">
-                    <img src={require('../../assets/images/home_vector_img.png')} alt=""/>
-                </div>
-            </div>
-            <div className="explore-div">
-                <a href="/"> EXPLORE THE POSSIBILITIES</a>
-            </div>
-        </header>
     )
 }
 
--- src/components/onbording/registration/registration.js
+++ src/components/onbording/registration/registration.js
@@ -1,7 +1,4 @@
 import * as React from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css'
-import '../../../assets/css/bootstrap.css';
-
 
 const Registraion=()=>{
     return (
